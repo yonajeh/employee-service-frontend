986cd05154647d85a72ca38888e2db72f6c342cc
Youssef NAJEH <yonajehlux@gmail.com>
2022-05-02 23:41:13 +0200

    add angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { GetAllComponent } from './get-all/get-all.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {AppComponent} from './app.component';
+import {GetAllComponent} from './get-all/get-all.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {HttpClientModule} from "@angular/common/http";
-import { CreateComponent } from './create/create.component';
+import {CreateComponent} from './create/create.component';
 import {FormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: 'all', component: GetAllComponent},
+  {path: 'create', component: CreateComponent}
+];
+
 
 @NgModule({
   declarations: [
@@ -17,6 +24,7 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     NgbModule,
     FormsModule,
     FontAwesomeModule,
@@ -25,4 +33,5 @@ import {FormsModule} from "@angular/forms";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
